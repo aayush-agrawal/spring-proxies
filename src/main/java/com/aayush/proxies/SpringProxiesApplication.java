c6d17663e14ad41a8d2cd628201fc76e1410cf6b
Aayush Agrawal <aayagraw@adobe.com>
2024-07-29 13:57:49 +0530

    Create proxy using 'ProxyFactory'

--- src/main/java/com/aayush/proxies/SpringProxiesApplication.java
+++ src/main/java/com/aayush/proxies/SpringProxiesApplication.java
@@ -4,6 +4,8 @@ import com.aayush.proxies.annotation.MyTransactional;
 import com.aayush.proxies.service.CustomerService;
 import com.aayush.proxies.service.DefaultCustomerService;
 import java.lang.reflect.Proxy;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,23 +22,25 @@ public class SpringProxiesApplication {
 	ApplicationRunner applicationRunner() {
 		return args -> {
 			var target = new DefaultCustomerService();
-
-			var proxyInstance = (CustomerService) Proxy.newProxyInstance(target.getClass().getClassLoader(),
-				target.getClass().getInterfaces(),
-				(proxy, method, methodArgs) -> {
-					try {
-						if (method.getAnnotation(MyTransactional.class) != null) {
-							System.out.println("Transaction started");
-						}
-						return method.invoke(target, methodArgs);
-					} finally {
-						if (method.getAnnotation(MyTransactional.class) != null) {
-							System.out.println("Transaction committed");
-						}
+			var pf = new ProxyFactory();
+			pf.setTarget(target);
+			pf.setInterfaces( target.getClass().getInterfaces() );
+			MethodInterceptor interceptor = invocation -> {
+				var method = invocation.getMethod();
+				var arguments = invocation.getArguments();
+				try {
+					if (method.getAnnotation(MyTransactional.class) != null) {
+						System.out.println("Transaction started");
+					}
+					return method.invoke(target, arguments);
+				} finally {
+					if (method.getAnnotation(MyTransactional.class) != null) {
+						System.out.println("Transaction committed");
 					}
 				}
-			);
-
+			};
+			pf.addAdvice(interceptor);
+			var proxyInstance = (CustomerService) pf.getProxy();
 			proxyInstance.create();
 		};
 	}
